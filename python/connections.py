bda52c7a70411e5cd8aa9645de8483adc3688d63
kgvlasov <88287301+kgvlasov@users.noreply.github.com>
2023-03-11 03:16:51 +0600

    Update connections.py

--- python/connections.py
+++ python/connections.py
@@ -1,7 +1,7 @@
 from airflow.hooks.clickhouse_hook import ClickhouseHook
 
 def get_clickhouse_client():
-    clickhouse_conn_id = 'clickhouse_dev'
+    clickhouse_conn_id = 'clickhouse'
     ch_hook = ClickhouseHook(clickhouse_conn_id)
     print(ch_hook)
     try:
